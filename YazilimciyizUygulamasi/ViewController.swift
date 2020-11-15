764ed6f79c81fe8d6724aa9c21d3d12a53733f57
Ayse Nur Bakırcı <bakirciaysenur@icloud.com>
2020-11-15 23:31:08 +0300

    Initial Commit

---
+++ YazilimciyizUygulamasi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YazilimciyizUygulamasi
+//
+//  Created by Ayşe Nur Bakırcı on 15.11.2020.
+//  Copyright © 2020 Ayşe Nur Bakırcı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
